291546c38d8c7791b57b02d7a7f311dc1ae380ae
Gautham Nayak <gauthamg2000@gmail.com>
2022-04-28 00:01:37 +0530

    implemented Asc & Dsc toggle with persistance

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./List.css";
 import Item from "./Item";
 import { StoryType } from "../types";
 import { useState } from "react";
+import usePersistence from "../hooks/usePersistence";
 
 type ListProps = {
   listOfItems: Array<StoryType>;
@@ -15,19 +16,30 @@ const SORT_COLUMNS = {
 };
 
 const List = ({ listOfItems }: ListProps) => {
-  const [sortedList, setSortedList] = useState({ data: listOfItems, flag: true });
-  const [arrowDirection, setArrowDirection] = useState("");
+  const [sortDirection, setSortDirection] = usePersistence("sortDirection", "true");
+  const [sortColumnName, setSortColumnName] = usePersistence("sortColumnName", "title");
+  const [sortedList, setSortedList] = useState({ data: sortDirection === "true" ? listOfItems.sort(sortByColumn(sortColumnName)).reverse() :listOfItems.sort(sortByColumn(sortColumnName)), flag: sortDirection === "false" ? true : false });
+
+
+  function sortByColumn(column: string) {
+    switch (column) {
+      case "title": return (a: StoryType, b: StoryType) => a?.title?.localeCompare(b?.title);
+      case "url": return (a: StoryType, b: StoryType) => a?.url?.localeCompare(b?.url);
+      case "author": return (a: StoryType, b: StoryType) => a?.author?.localeCompare(b?.author);
+    }
+  }
 
 
   function handleSort(column: "title" | "url" | "author") {
-    const sortedListOfItems = { data: sortedList.flag ? [...sortedList.data.sort(SORT_COLUMNS[column])] : [...sortedList.data.sort(SORT_COLUMNS[column])].reverse(), flag: !sortedList.flag };
-    setArrowDirection(sortedListOfItems.flag ? "false" : "true");
+    const sortedListOfItems = { data: sortDirection === "true" ? [...sortedList.data.sort(SORT_COLUMNS[column])] : [...sortedList.data.sort(SORT_COLUMNS[column])].reverse(), flag: !sortedList.flag };
+    setSortDirection(sortedList.flag.toString() === "true" ? "true" : "false");
+    setSortColumnName(column);
     setSortedList(sortedListOfItems);
   }
 
   return (
     <div>
-      <h4 style={{marginTop : 100}}>{arrowDirection == "" ? " " : arrowDirection == "true" ? " ↑ Ascending": " ↓ Descending"}</h4>
+      <h4 style={{ marginTop: 100 }}>{sortDirection === "" ? " " : sortDirection === "false" ? " ↑ Ascending  " : " ↓ Descending  "}({sortColumnName})</h4>
       <table>
         <thead>
           <tr>
