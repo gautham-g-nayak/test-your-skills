717d690e446bca58d8826ada96a74f3c1822c04c
Gautham Nayak <gauthamg2000@gmail.com>
2022-04-29 20:28:37 +0530

    session 16

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,55 +3,78 @@ import "./List.css";
 import Item from "./Item";
 import { StoryType } from "../types";
 import { useState } from "react";
-import usePersistence from "../hooks/usePersistence";
 
 type ListProps = {
   listOfItems: Array<StoryType>;
 };
 
-const SORT_COLUMNS = {
+const SORT_COLUMNS: any = {
   title: (a: StoryType, b: StoryType) => a?.title?.localeCompare(b?.title),
   url: (a: StoryType, b: StoryType) => a?.url?.localeCompare(b?.url),
   author: (a: StoryType, b: StoryType) => a?.author?.localeCompare(b?.author),
+  num_comments: (a: StoryType, b: StoryType) =>
+    a?.num_comments - b?.num_comments,
+};
+
+const CustomSortHeaders = ({ headers, onClick, sortInfo }: any) => {
+  return headers.map(([column, displayName]: any) => (
+    <CustomSortHeader
+      key={column}
+      column={column}
+      displayName={displayName}
+      onClick={onClick}
+      sortInfo={sortInfo}
+    />
+  ));
+};
+
+const CustomSortHeader = ({ column, displayName, onClick, sortInfo }: any) => {
+  return (
+    <th onClick={() => onClick(column)}>
+      {displayName}
+      {"        "}
+      {sortInfo.column === column && (sortInfo.isDesc ? "↓" : "↑")}
+    </th>
+  );
 };
 
 const List = ({ listOfItems }: ListProps) => {
-  const [sortDirection, setSortDirection] = usePersistence("sortDirection", "true");
-  const [sortColumnName, setSortColumnName] = usePersistence("sortColumnName", "title");
-  const [sortedList, setSortedList] = useState({ data: sortDirection === "true" ? listOfItems.sort(sortByColumn(sortColumnName)).reverse() :listOfItems.sort(sortByColumn(sortColumnName)), flag: sortDirection === "false" ? true : false });
-
-
-  function sortByColumn(column: string) {
-    switch (column) {
-      case "title": return (a: StoryType, b: StoryType) => a?.title?.localeCompare(b?.title);
-      case "url": return (a: StoryType, b: StoryType) => a?.url?.localeCompare(b?.url);
-      case "author": return (a: StoryType, b: StoryType) => a?.author?.localeCompare(b?.author);
-    }
+  const [sortInfo, setSortInfo] = useState(() => {
+    console.log("Initializing sort info state");
+    return { column: "none", isDesc: false };
+  });
+  const newListOfItems = [...listOfItems];
+  newListOfItems.sort(SORT_COLUMNS[sortInfo.column]);
+  if (sortInfo.isDesc) {
+    newListOfItems.reverse();
   }
 
-
-  function handleSort(column: "title" | "url" | "author") {
-    const sortedListOfItems = { data: sortDirection === "true" ? [...sortedList.data.sort(SORT_COLUMNS[column])] : [...sortedList.data.sort(SORT_COLUMNS[column])].reverse(), flag: !sortedList.flag };
-    setSortDirection(sortedList.flag.toString() === "true" ? "true" : "false");
-    setSortColumnName(column);
-    setSortedList(sortedListOfItems);
+  function handleSort(column: "title" | "url" | "author" | "num_comments") {
+    setSortInfo((prev) => {
+      return { column, isDesc: !prev.isDesc };
+    });
   }
 
   return (
     <div>
-      <h4 style={{ marginTop: 100 }}>{sortDirection === "" ? " " : sortDirection === "false" ? " ↑ Ascending  " : " ↓ Descending  "}({sortColumnName})</h4>
       <table>
         <thead>
           <tr>
-            <th onClick={() => handleSort("title")}>Title</th>
-            <th onClick={() => handleSort("url")}>URL</th>
-            <th onClick={() => handleSort("author")}>Author</th>
-            <th>Comments</th>
+            <CustomSortHeaders
+              headers={[
+                ["title", "Title"],
+                ["url", "URL"],
+                ["author", "Author"],
+                ["num_comments", "Comments"],
+              ]}
+              onClick={handleSort}
+              sortInfo={sortInfo}
+            />
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {sortedList.data.map((item) => (
+          {newListOfItems.map((item) => (
             <Item key={item.objectID} item={item} />
           ))}
         </tbody>
